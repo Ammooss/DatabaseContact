386f86efcb7b2aafaf74e6573d7fd4171b861f74
Maxence Rougé <rouge.maxence31@gmail.com>
2021-11-25 10:30:06 +0100

    [CREATE] Project

--- DatabaseContact/DatabaseContact.pro
+++ DatabaseContact/DatabaseContact.pro
@@ -1,4 +1,6 @@
 TEMPLATE = subdirs
 
 SUBDIRS += \
-    Contacts
+    Contact \
+    Contacts \
+    tests-contacts
---
+++ DatabaseContact/Tests-Contacts/tst_mytestscontacts.cpp
@@ -0,0 +1,48 @@
+#include <QtTest>
+#include <QCoreApplication>
+
+// add necessary includes here
+
+class MyTestsContacts : public QObject
+{
+    Q_OBJECT
+
+public:
+    MyTestsContacts();
+    ~MyTestsContacts();
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void test_case1();
+
+};
+
+MyTestsContacts::MyTestsContacts()
+{
+
+}
+
+MyTestsContacts::~MyTestsContacts()
+{
+
+}
+
+void MyTestsContacts::initTestCase()
+{
+
+}
+
+void MyTestsContacts::cleanupTestCase()
+{
+
+}
+
+void MyTestsContacts::test_case1()
+{
+
+}
+
+QTEST_MAIN(MyTestsContacts)
+
+#include "tst_mytestscontacts.moc"
