a8295f1c328bdfbe47805302ebefd1b14cb38d2e
Maxence Rougé <rouge.maxence31@gmail.com>
2021-11-30 17:01:37 +0100

    [Add] Init database

--- DatabaseContact/Contacts/database.cpp
+++ DatabaseContact/Contacts/database.cpp
@@ -2,5 +2,14 @@
 
 Database::Database()
 {
-
+    _Database = QSqlDatabase::addDatabase("QSQLITE");
+    _Database.setDatabaseName(""); //Database path (Appdata folder)
+    if (!_Database.open())
+     {
+        qDebug() << "Error: connection with database failed";
+     }
+     else
+     {
+        qDebug() << "Database: connection ok";
+     }
 }
--- DatabaseContact/Contacts/database.h
+++ DatabaseContact/Contacts/database.h
@@ -13,12 +13,21 @@ class Database
     QSqlDatabase _Database;
 public:
     Database();
-    void addRow(const QString& ID, const QString& GUI, const QString& firstname, const QString& lastname,
+    void addRow(const QString& ID, const QString& GUID, const QString& firstname, const QString& lastname,
                 const QString& email, const QString& tel, const QString& category, const QString& city,
-                const QString& birth_date, const QString& list, const QString& company);
-    QStringList getFromName(QString& filename);
-    QStringList getFromExt(QString& fileextension);
-    QStringList transformQueryToStringList(QSqlQuery query);
+                const QString& birth_date, const QString& country, const QString& list, const QString& company);
+    QStringList getID(QString& ID);
+    QStringList getGUID(QString& GUID);
+    QStringList getFirstName(QString& firstname);
+    QStringList getLastName(QString& lastname);
+    QStringList getEmail(QString& email);
+    QStringList getTel(QString& tel);
+    QStringList getCategory(QString& category);
+    QStringList getCity(QString& city);
+    QStringList getBirthDate(QString& birth_date);
+    QStringList getCountry(QString& country);
+    QStringList getList(QString& list);
+    QStringList getCompany(QString& company);
 };
 
 #endif // DATABASE_H
